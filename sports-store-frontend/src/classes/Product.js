dccb448105e2aa93a33d485048ebda932395676f
gabriel-vr <gvicente53@gmail.com>
2022-05-25 15:44:32 -0300

    feat: classes to handle products and users

---
+++ sports-store-frontend/src/classes/Product.js
@@ -0,0 +1,102 @@
+export default class Product {
+
+    constructor() {
+        this.id = "";
+        this.name = "";
+        this.description = "";
+        this.price = "";
+        this.quantityStock = "";
+        this.quantitySold = "";
+        this.image = "";
+        this.image3d = false;
+    }
+    constructor(id, name, description, price, quantityStock, quantitySold, image, image3d) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantityStock = quantityStock;
+        this.quantitySold = quantitySold;
+        this.image = image;
+        this.image3d = image3d;
+    }
+    setName(value) {
+        this.name = value;
+    }
+    setDescription(value) {
+        this.description = value;
+    }
+    setPrice(value) {
+        this.price = value;
+    }
+    setQuantityStock(value) {
+        this.quantityStock = value;
+    }
+    setQuantitySold(value) {
+        this.quantitySold = value;
+    }
+    setImage(value){
+        this.image = value;
+    }
+    setImage3d(value){
+        this.image3d = value;
+    }
+
+    async create() {
+        //faz criação do objeto no database caso não exista
+        return this;
+    }
+
+
+    async update() {
+        //faz update no database aqui
+        //retorna objeto atualizado
+        return this;
+    }
+
+    async delete() {
+        //faz deleção do objeto no database
+    }
+
+    static async getProducts() {
+        let products = []
+        for (const product of productsList) {
+            const productObj = new Product(...product);
+            products.push(productObj);
+        }
+        return products;
+    }
+
+
+    static async getproductById(id) {
+        const products = productsList.filter(obj => obj.id === id);
+        if (products.length === 0)
+            return null;
+        return new Product(...products[0]);
+    }
+}
+
+
+let productsList = [
+    {
+        id: 1,
+        name: "Nike Air Shoe",
+        description: "Nice shoe :)",
+        price: 100.00,
+        quantityStock: 50,
+        quantitySold: 10,
+        image: "",
+        image3d: "",
+    },
+    {
+        id: 2,
+        name: "Puma Ball",
+        description: "Nice ball :)",
+        price: 30.00,
+        quantityStock: 100,
+        quantitySold: 20,
+        image: "",
+        image3d: "",
+    },
+
+]
---
+++ sports-store-frontend/src/classes/User.js
@@ -0,0 +1,128 @@
+export default class User {
+
+    constructor() {
+        this.id = "";
+        this.name = "";
+        this.email = "";
+        this.password = "";
+        this.phone = "";
+        this.address = "";
+        this.profilePicture = "";
+        this.isAdmin = false;
+    }
+    constructor(id, name, email, password, phone, address, profilePicture, isAdmin) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.address = address;
+        this.profilePicture = profilePicture;
+        this.isAdmin = isAdmin;
+    }
+    setName(value) {
+        this.name = value;
+    }
+    setEmail(value) {
+        this.email = value;
+    }
+    setPassword(value) {
+        this.password = value;
+    }
+    setPhone(value) {
+        this.phone = value;
+    }
+    setAddress(value) {
+        this.address = value;
+    }
+    setProfilePicture(value){
+        this.profilePicture = value;
+    }
+
+    async create() {
+        //faz criação do objeto no database caso não exista
+        return this;
+    }
+
+
+    async update() {
+        //faz update no database aqui
+        //retorna objeto atualizado
+        return this;
+    }
+
+    async delete() {
+        //faz deleção do objeto no database
+    }
+
+    async login() {
+        let user = false;
+        usersList.forEach(obj => {
+            if(obj.email === this.email && obj.password === this.password) {
+                localStorage.setItem("user", JSON.stringify(obj));
+                exists = true;
+                return;
+            }
+        })
+        return exists;
+    }
+
+    static async getUsers() {
+        let users = []
+        for (const user of usersList) {
+            const userObj = new User(...user);
+            users.push(userObj);
+        }
+        return users;
+    }
+
+    static async getClients() {
+        let users = []
+        for (const user of usersList.filter(obj => !obj.isAdmin)) {
+            const userObj = new User(...user);
+            users.push(userObj);
+        }
+        return users;
+    }
+
+    static async getAdmins() {
+        let users = []
+        for (const user of usersList.filter(obj => obj.isAdmin)) {
+            const userObj = new User(...user);
+            users.push(userObj);
+        }
+        return users;
+    }
+
+    static async getUserById(id) {
+        const users = usersList.filter(obj => obj.id === id);
+        if (users.length === 0)
+            return null;
+        return new User(...users[0]);
+    }
+}
+
+
+let usersList = [
+    {
+        id: 1,
+        name: "Admin",
+        email: "admin@admin.com",
+        password: "admin",
+        phone: "+5518996347405",
+        address: "Rua das Saudades",
+        profilePicture: "",
+        isAdmin: true,
+    },
+    {
+        id: 2,
+        name: "Gabriel",
+        email: "gabriel_vr@usp.br",
+        password: "123456",
+        phone: "+5518996347405",
+        address: "Rua das Saudades",
+        profilePicture: "",
+        isAdmin: false,
+    },
+
+]
