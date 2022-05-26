461eff6f546f75f8efcb7cdb9cfa55a11ce2137e
gabriel-vr <gvicente53@gmail.com>
2022-05-26 09:32:10 -0300

    fix: methods

--- sports-store-frontend/src/classes/Product.js
+++ sports-store-frontend/src/classes/Product.js
@@ -1,13 +1,18 @@
+const male = "male";
+const female = "female";
+const kids = "kids";
+
 export default class Product {
-    constructor(id, name, description, price, quantityStock, quantitySold, image, image3d) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.quantityStock = quantityStock;
-        this.quantitySold = quantitySold;
-        this.image = image;
-        this.image3d = image3d;
+    constructor(data) {
+        this.id = data.id;
+        this.name = data.name;
+        this.description = data.description;
+        this.price = data.price;
+        this.quantityStock = data.quantityStock;
+        this.quantitySold = data.quantitySold;
+        this.image = data.image;
+        this.image3d = data.image3d;
+        this.category = data.category;
     }
     setName(value) {
         this.name = value;
@@ -30,6 +35,10 @@ export default class Product {
     setImage3d(value){
         this.image3d = value;
     }
+    setCategory(value){
+        this.category = value;
+    }
+
 
     async create() {
         //faz criação do objeto no database caso não exista
@@ -48,20 +57,46 @@ export default class Product {
     }
 
     static async getProducts() {
+        let products = {}
+        products[male] = await Product.getMaleProducts()
+        products[female] = await Product.getFemaleProducts()
+        products[kids] = await Product.getKidsProducts()
+        return products;
+    }
+
+    static async getMaleProducts() {
         let products = []
-        for (const product of productsList) {
-            const productObj = new Product(...product);
+        for (const product of productsList.filter(obj => obj.category === male)) {
+            const productObj = new Product(product);
+            products.push(productObj);
+        }
+        return products;
+    }
+    static async getFemaleProducts() {
+        let products = []
+        for (const product of productsList.filter(obj => obj.category === female)) {
+            const productObj = new Product(product);
+            products.push(productObj);
+        }
+        return products;
+    }
+
+    static async getKidsProducts() {
+        let products = []
+        for (const product of productsList.filter(obj => obj.category === kids)) {
+            const productObj = new Product(product);
             products.push(productObj);
         }
         return products;
     }
 
 
+
     static async getproductById(id) {
         const products = productsList.filter(obj => obj.id === id);
         if (products.length === 0)
             return null;
-        return new Product(...products[0]);
+        return new Product(products[0]);
     }
 }
 
@@ -74,8 +109,9 @@ let productsList = [
         price: 100.00,
         quantityStock: 50,
         quantitySold: 10,
-        image: "",
-        image3d: "",
+        image: "/assets/NikeShoe.png",
+        image3d: "/assets/NikeShoe.stl",
+        category: male,
     },
     {
         id: 2,
@@ -84,8 +120,21 @@ let productsList = [
         price: 30.00,
         quantityStock: 100,
         quantitySold: 20,
-        image: "",
+        image: "/assets/PumaBall.png",
         image3d: "",
+        category: kids
     },
+    {
+        id: 3,
+        name: "Asics Shoes",
+        price: 500,
+        description: "O tênis de tênis GEL-RESOLUTION ™ 8 promove um passo ágil com uma sensação de proximidade ao solo da quadra. A parte superior do FLEXION FIT ™ fornece suporte de ajuste de forma com a integração da tecnologia DYNAWALL ™, que oferece maior estabilidade no mediopé durante movimentos laterais e cobertura costa a costa.",
+        image: "AsicsShoes.png",
+        quantityStock: 500,
+        quantitySold: 100,
+        image: "/assets/AsicsShoes.png",
+        image3d: "",
+        category: female,
+    }
 
 ]
--- sports-store-frontend/src/classes/User.js
+++ sports-store-frontend/src/classes/User.js
@@ -1,13 +1,13 @@
 export default class User {
-    constructor(id, name, email, password, phone, address, profilePicture, isAdmin) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.phone = phone;
-        this.address = address;
-        this.profilePicture = profilePicture;
-        this.isAdmin = isAdmin;
+    constructor(data) {
+        this.id = data.id;
+        this.name = data.name;
+        this.email = data.email;
+        this.password = data.password;
+        this.phone = data.phone;
+        this.address = data.address;
+        this.profilePicture = data.profilePicture;
+        this.isAdmin = data.isAdmin;
     }
     setName(value) {
         this.name = value;
@@ -49,7 +49,7 @@ export default class User {
         usersList.forEach(obj => {
             if(obj.email === email && obj.password === password) {
                 localStorage.setItem("user", JSON.stringify(obj));
-                user = new User(...obj);
+                user = new User(obj);
                 return;
             }
         })
@@ -59,10 +59,14 @@ export default class User {
             throw new Error("Usuário não encontrado!");
     }
 
+    static async logout() {
+        localStorage.clear();
+    }
+
     static async getUsers() {
         let users = []
         for (const user of usersList) {
-            const userObj = new User(...user);
+            const userObj = new User(user);
             users.push(userObj);
         }
         return users;
@@ -71,7 +75,7 @@ export default class User {
     static async getClients() {
         let users = []
         for (const user of usersList.filter(obj => !obj.isAdmin)) {
-            const userObj = new User(...user);
+            const userObj = new User(user);
             users.push(userObj);
         }
         return users;
@@ -80,7 +84,7 @@ export default class User {
     static async getAdmins() {
         let users = []
         for (const user of usersList.filter(obj => obj.isAdmin)) {
-            const userObj = new User(...user);
+            const userObj = new User(user);
             users.push(userObj);
         }
         return users;
@@ -90,7 +94,7 @@ export default class User {
         const users = usersList.filter(obj => obj.id === id);
         if (users.length === 0)
             return null;
-        return new User(...users[0]);
+        return new User(users[0]);
     }
 }
 
@@ -115,6 +119,66 @@ let usersList = [
         address: "Rua das Saudades",
         profilePicture: "",
         isAdmin: false,
+    },
+    {
+        id: 3,
+        name: "Joao",
+        phone: "991318888",
+        adress: "rua Jacinto 884",
+        email: "joao@gmail.com",
+        password: "1234",
+        profilePicture: "",
+        isAdmin: false,
+    },
+    {
+        id: 4,
+        name: "Jose",
+        phone: "98765766",
+        adress: "rua Salomao 333",
+        email: "jose@gmail.com",
+        senha: "1234",
+        profilePicture: "",
+        isAdmin: false,
+    },
+    {
+        id: 5,
+        name: "Jorge",
+        phone: "976843232",
+        adress: "rua Trabalhador 123",
+        email: "jorge@gmail.com",
+        senha: "1234",
+        profilePicture: "",
+        isAdmin: false,
+    },
+    {
+        id: 6,
+        name: "Pedro",
+        phone: "976843232",
+        adress: "rua Jacinto 450",
+        email: "pedro@gmail.com",
+        senha: "1234",
+        profilePicture: "",
+        isAdmin: true,
+    },
+    {
+        id: 7,
+        name: "Paulo",
+        phone: "976843232",
+        adress: "rua Salomao 555",
+        email: "paulo@gmail.com",
+        senha: "1234",
+        profilePicture: "",
+        isAdmin: true,
+    },
+    {
+        id: 8,
+        name: "Pepe",
+        phone: "976843232",
+        adress: "rua Trabalhador 999",
+        email: "pepe@gmail.com",
+        senha: "1234",
+        profilePicture: "",
+        isAdmin: true,
     },
 
 ]
