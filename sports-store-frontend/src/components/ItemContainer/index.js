7d9cb7bc399574b0bcb1946c945b668d89188452
VictorLutes <victorlutes@usp.br>
2022-06-29 18:50:23 -0300

    added cart

--- sports-store-frontend/src/components/ItemContainer/index.js
+++ sports-store-frontend/src/components/ItemContainer/index.js
@@ -1,22 +1,30 @@
 import React, {useState, useEffect} from "react";
 import './styles.css'
 import Button from "../Button"
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import User from "../../classes/User";
+import Cart from "../../classes/Cart";
 
 export default function ItemContainer({
     item,
+
 }) {
+    const navigate = useNavigate();
     const [user, setUser] = useState({})
     const params = useParams();
     useEffect(() => {
         const loadAll = async () => {
-            const user = await User.getUserById(params.userID);
+            const user = await User.getUserById(parseInt(params.userID));
             setUser(user);
         }
     
       loadAll();
     }, [])
+
+    const addItemCart = () => {
+        Cart.insertItem(item.id);
+        navigate(`/client-shipping/${parseInt(params.userID)}`);
+    }
 
     return (
         <div>
@@ -24,7 +32,7 @@ export default function ItemContainer({
                 <img src={item.image} alt="item"></img>
                 <p>{item.name}</p>
                 <p>R${item.price}</p>
-                <Button orange link to={"/client-shipping/"+user.id}>
+                <Button orange onClick={addItemCart}>
                     ADD TO CART
                 </Button>
                 <Button purple link to={"/client-productPage/"+user.id+"/"+item.id}>
