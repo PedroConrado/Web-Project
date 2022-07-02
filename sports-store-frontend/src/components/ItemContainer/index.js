7b8121d24986af1490571f3a3ce86a9e21d18d6b
gabriel-vr <gvicente53@gmail.com>
2022-07-02 16:06:18 -0300

    Revert "merge"
    This reverts commit 958a15f9e51f951db21cfc9e00a958c0c80a975d, reversing
    changes made to 79042d0f4373e52ef65e804177d94718ffdaf54b.

--- sports-store-frontend/src/components/ItemContainer/index.js
+++ sports-store-frontend/src/components/ItemContainer/index.js
@@ -1,13 +1,15 @@
 import React, {useState, useEffect} from "react";
 import './styles.css'
 import Button from "../Button"
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import User from "../../classes/User";
+import Cart from "../../classes/Cart";
 
 export default function ItemContainer({
     item,
 
 }) {
+    const navigate = useNavigate();
     const [user, setUser] = useState({})
     const params = useParams();
     useEffect(() => {
@@ -19,13 +21,18 @@ export default function ItemContainer({
       loadAll();
     }, [])
 
+    const addItemCart = () => {
+        Cart.insertItem(item.id);
+        navigate(`/client-shipping/${parseInt(params.userID)}`);
+    }
+
     return (
         <div>
             <div className="itemContainer">
                 <img src={item.image} alt="item"></img>
                 <p>{item.name}</p>
                 <p>R${item.price}</p>
-                <Button orange link to={"/client-shipping/"+user.id}>
+                <Button orange onClick={addItemCart}>
                     ADD TO CART
                 </Button>
                 <Button purple link to={"/client-productPage/"+user.id+"/"+item.id}>
